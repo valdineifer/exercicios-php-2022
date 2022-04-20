53449d9e828a6c0ec8a0f5d42175d83a5a0bab0e
Valdinei Ferreira <valdineifer.dev@outlook.com>
2022-04-20 20:50:58 -0300

    Ajusta indentação de classe Battlefield

--- src/War/GamePlay/Battlefield.php
+++ src/War/GamePlay/Battlefield.php
@@ -10,54 +10,54 @@ use Galoa\ExerciciosPhp2022\War\GamePlay\Country\CountryInterface;
 class Battlefield implements BattlefieldInterface
 {
 
-    public function rollDice(CountryInterface $country, bool $isAttacking): array
-    {
-        $values = [];
-        $numTroops = $country->getNumberOfTroops();
+  public function rollDice(CountryInterface $country, bool $isAttacking): array
+  {
+    $values = [];
+    $numTroops = $country->getNumberOfTroops();
 
-        if ($isAttacking)
-            $numTroops--;
+    if ($isAttacking)
+      $numTroops--;
 
-        for ($i = 0; $i < $numTroops; $i++)
-            array_push($values, rand(1, 6));
+    for ($i = 0; $i < $numTroops; $i++)
+      array_push($values, rand(1, 6));
 
-        return $values;
+    return $values;
+  }
+
+  public function computeBattle(
+    CountryInterface $attackingCountry,
+    array $attackingDice,
+    CountryInterface $defendingCountry,
+    array $defendingDice
+  ): void {
+    rsort($attackingDice);
+    rsort($defendingDice);
+
+    $attackingPoints = 0;
+    $defendingPoints = 0;
+
+    while (count($attackingDice) > 0 && count($defendingDice) > 0) {
+      if ($attackingDice[0] > $defendingDice[0]) {
+        $attackingPoints++;
+      } else {
+        $defendingPoints++;
+      }
+
+      unset($attackingDice[0]);
+      unset($defendingDice[0]);
     }
 
-    public function computeBattle(
-        CountryInterface $attackingCountry,
-        array $attackingDice,
-        CountryInterface $defendingCountry,
-        array $defendingDice
-    ): void {
-        rsort($attackingDice);
-        rsort($defendingDice);
+    $attackingCountry->killTroops($defendingPoints);
+    $defendingCountry->killTroops($attackingPoints);
 
-        $attackingPoints = 0;
-        $defendingPoints = 0;
-
-        while (count($attackingDice) > 0 && count($defendingDice) > 0) {
-            if ($attackingDice[0] > $defendingDice[0]) {
-                $attackingPoints++;
-            } else {
-                $defendingPoints++;
-            }
-
-            unset($attackingDice[0]);
-            unset($defendingDice[0]);
-        }
-
-        $attackingCountry->killTroops($defendingPoints);
-        $defendingCountry->killTroops($attackingPoints);
-
-        if (count($attackingDice) > count($defendingDice)) {
-            $attackingCountry->conquer($defendingCountry);
-        } else if (count($attackingDice) < count($defendingDice)) {
-            $defendingCountry->conquer($attackingCountry);
-        } else if ($attackingPoints > $defendingPoints) {
-            $attackingCountry->conquer($defendingCountry);
-        } else if ($attackingPoints < $defendingPoints) {
-            $defendingCountry->conquer($attackingCountry);
-        }
+    if (count($attackingDice) > count($defendingDice)) {
+      $attackingCountry->conquer($defendingCountry);
+    } else if (count($attackingDice) < count($defendingDice)) {
+      $defendingCountry->conquer($attackingCountry);
+    } else if ($attackingPoints > $defendingPoints) {
+      $attackingCountry->conquer($defendingCountry);
+    } else if ($attackingPoints < $defendingPoints) {
+      $defendingCountry->conquer($attackingCountry);
     }
+  }
 }
