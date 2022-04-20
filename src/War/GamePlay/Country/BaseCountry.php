710343cbe246d643f39ba278aeef0a06e76f1400
Valdinei Ferreira <valdineifer.dev@outlook.com>
2022-04-19 23:34:44 -0300

    WIP: Implementa métodos para países a partir da interface

--- src/War/GamePlay/Country/BaseCountry.php
+++ src/War/GamePlay/Country/BaseCountry.php
@@ -5,7 +5,8 @@ namespace Galoa\ExerciciosPhp2022\War\GamePlay\Country;
 /**
  * Defines a country, that is also a player.
  */
-class BaseCountry implements CountryInterface {
+class BaseCountry implements CountryInterface
+{
 
   /**
    * The name of the country.
@@ -15,13 +16,49 @@ class BaseCountry implements CountryInterface {
   protected $name;
 
   /**
+   * The number of troops
+   * 
+   * @var int
+   */
+  protected $numberOfTroops;
+
+  /**
+   * Indicates if the country was conquered or not
+   * 
+   * @var bool
+   */
+  protected $conquered;
+
+  /**
    * Builder.
    *
    * @param string $name
    *   The name of the country.
    */
-  public function __construct(string $name) {
+  public function __construct(string $name)
+  {
     $this->name = $name;
+    $this->numberOfTroops = 3;
+    $this->conquered = false;
   }
 
+  public function getName(): string
+  {
+    return $this->name;
+  }
+
+  public function getNumberOfTroops(): int
+  {
+    return $this->numberOfTroops;
+  }
+
+  public function isConquered(): bool
+  {
+    return $this->conquered;
+  }
+
+  public function killTroops(int $killedTroops): void
+  {
+    $this->numberOfTroops -= $killedTroops;
+  }
 }
