674bdaa67152b8c089a16a597829241621f19f0f
Valdinei Ferreira <valdineifer.dev@outlook.com>
2022-04-19 23:03:39 -0300

    Implementa computação dos dados de batalha e corrige rolagem dos dados

--- src/War/GamePlay/Battlefield.php
+++ src/War/GamePlay/Battlefield.php
@@ -13,10 +13,47 @@ class Battlefield implements BattlefieldInterface
     public function rollDice(CountryInterface $country, bool $isAttacking): array
     {
         $values = [];
+        $numTroops = $country->getNumberOfTroops();
 
-        for ($i = 0; $i < 6; $i++)
+        if ($isAttacking)
+            $numTroops--;
+
+        for ($i = 0; $i < $numTroops; $i++)
             array_push($values, rand(1, 6));
 
         return $values;
+    }
+
+    public function computeBattle(
+        CountryInterface $attackingCountry,
+        array $attackingDice,
+        CountryInterface $defendingCountry,
+        array $defendingDice
+    ): void {
+        rsort($attackingDice);
+        rsort($defendingDice);
+
+        $attackingPoints = 0;
+        $defendingPoints = 0;
+
+        while (count($attackingDice) > 0 && count($defendingDice) > 0) {
+            if ($attackingDice[0] > $defendingDice[0]) {
+                $attackingPoints++;
+            } else {
+                $defendingPoints++;
+            }
+
+            unset($attackingDice[0]);
+            unset($defendingDice[0]);
+        }
+
+        $attackingCountry->killTroops($defendingPoints);
+        $defendingCountry->killTroops($attackingPoints);
+
+        if (count($attackingDice) > count($defendingDice)) {
+            $attackingCountry->conquer($defendingCountry);
+        } else if (count($attackingDice) < count($defendingDice)) {
+            $defendingCountry->conquer($attackingCountry);
+        }
     }
 }
