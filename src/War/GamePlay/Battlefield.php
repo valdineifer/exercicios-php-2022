5c5cfaf061afad254fedd91870ba04ef58db1d7b
Valdinei Ferreira <valdineifer.dev@outlook.com>
2022-04-19 22:05:06 -0300

    Implementa método para rolagem de dado

--- src/War/GamePlay/Battlefield.php
+++ src/War/GamePlay/Battlefield.php
@@ -7,5 +7,16 @@ use Galoa\ExerciciosPhp2022\War\GamePlay\Country\CountryInterface;
 /**
  * A manager that will roll the dice and compute the winners of a battle.
  */
-class Battlefield implements BattlefieldInterface {
+class Battlefield implements BattlefieldInterface
+{
+
+    public function rollDice(CountryInterface $country, bool $isAttacking): array
+    {
+        $values = [];
+
+        for ($i = 0; $i < 6; $i++)
+            array_push($values, rand(1, 6));
+
+        return $values;
+    }
 }
